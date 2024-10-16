2a65da0fa780b74ba7f4fdd9dc6f92f272957931
Nathan Wiebe Neufeldt <wn.nathan@gmail.com>
2024-10-15 21:29:20 -0400

    Update vendored message packages
    This updates the message packages vendored under the rclrs `vendor`
    module. The vendor_interfaces.py script was used for reproducibility.
    
    The existing packages – rcl_interfaces, rosgraph_msgs, and
    builtin_interfaces – are only modified in that they now use the
    std::ffi::c_void naming for c_void instead of the std::os::raw::c_void
    alias.
    
    The action_msgs and unique_identifier_msgs packages are newly added to
    enable adding action support to rclrs. action_msgs is needed for
    interaction with action goal info and statuses, and has a dependency on
    unique_identifier_msgs.

--- rclrs/src/vendor/mod.rs
+++ rclrs/src/vendor/mod.rs
@@ -2,6 +2,8 @@
 #![allow(dead_code)]
 #![allow(clippy::derive_partial_eq_without_eq)]
 
+pub mod action_msgs;
 pub mod builtin_interfaces;
 pub mod rcl_interfaces;
 pub mod rosgraph_msgs;
+pub mod unique_identifier_msgs;
